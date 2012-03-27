233477266689b9ff72381e809c479918006ef5fe
walas83 <michal.walas@gmail.com>
2012-03-27 18:03:08 +0200

    czyszczenie kesza

--- photka/cache/backend/dev/modules/autoCategory/templates/_list_td_stacked.php
+++
@@ -1,3 +0,0 @@
-<td colspan="2">
-  <?php echo __('%%id%% - %%name%%', array('%%id%%' => link_to($category->getId(), 'category_edit', $category), '%%name%%' => $category->getName()), 'messages') ?>
-</td>
