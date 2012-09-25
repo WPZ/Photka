39fb4bfca124c78f12831de56a382906c54328b1
walas83 <michal.walas@hurra.com>
2012-09-25 19:37:58 +0200

    wyszukiwanie po kategoriach + wyswietlanie opisu i kategori

--- photka/apps/frontend/modules/index/actions/actions.class.php
+++ photka/apps/frontend/modules/index/actions/actions.class.php
@@ -26,8 +26,18 @@ class indexActions extends sfActions
     if($request->getParameter('query') ){
       $this->forwardUnless($query = $request->getParameter('query'), 'photo', 'index');
 
-//    $this->photos = Doctrine_Core::getTable('Photo')->getForLuceneQuery($query);
-      $this->photos = Doctrine_Core::getTable('Photo')->createQuery()->where('name LIKE ?', "%$query%")->fetchArray();
+
+      $q = Doctrine_Query::create()
+        ->select('photo.name AS name, photo.path AS path,  photo.description AS description, photo_category_value.photo_id, category_value.value, category.name')
+        ->from('Photo photo')
+        ->innerJoin('photo.PhotoCategoryValues photo_category_value ON photo.id = photo_category_value.photo_id')
+        ->innerJoin('photo_category_value.CategoryValue  category_value ON photo_category_value.category_value_id = category_value.id')
+        ->innerJoin('category_value.Category AS category ON category_value.category_id = category.id')
+        ->where('name LIKE ?', "%$query%")
+        ->orwhere('category.name LIKE ?', "%$query%");
+// old way
+//        $this->photos = Doctrine_Core::getTable('Photo')->createQuery()->where('name LIKE ?', "%$query%")->fetchArray();
+        $this->photos = $q->fetchArray();
 
     } else {
        $this->photos = array();
--- photka/apps/frontend/modules/index/templates/searchSuccess.php
+++ photka/apps/frontend/modules/index/templates/searchSuccess.php
@@ -4,10 +4,23 @@
     <?php 
      $photos_arr =  $photos->getRawValue();
      foreach ($photos_arr as &$value) {
+         
+         $kat = '';
+         foreach ($value['PhotoCategoryValues'] as $PhotoCategoryValues) {
+            
+             $cv_arr = $PhotoCategoryValues['CategoryValue'];
+             $kat .= $cv_arr[value] . ' ';
+             $c_arr = $cv_arr['Category'];
+
+             $kat .= '(' . $c_arr[name] . '), ';
+             
+         }
 	echo '<li class="span3">';
 	    echo '<a href="#" class="thumbnail">';
                 echo '<img src="' . $value['path'] .  '" alt="">';
-                echo '<h5>' . $value['name'] . '</h5>';
+                echo '<h5> Nazwa pliku: ' . $value['name'] . '</h5>';
+                echo '<p> Opis: ' . $value['description'] . '</p>';
+                echo '<p> Kategorie: ' . $kat . '</p>';
              echo '</a>';
         echo '</li>';
      }  
